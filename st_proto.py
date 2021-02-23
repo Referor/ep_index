35a62214823f60b98bf33f5b8038ca63faa18e54
Андрей Батрименко <mbab@mbab.local>
2021-02-23 13:44:13 +0300

    Сводный индекс ++

--- st_proto.py
+++ st_proto.py
@@ -9,13 +9,19 @@ import plotly.graph_objs as go
 import plotly.express as px
 import matplotlib.pyplot as plt
 #https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4
-
-df_cov_sc=pd.read_excel('data/scaled_data.xlsx', index_col='month')
-fig = px.line(df_cov_sc, x='months', y='value', color='query', hover_name="query").for_each_trace(lambda t: t.update(name=t.name.split("=")[1]))
-fig.update_traces(mode='markers+lines')
-fig.update_layout(legend_traceorder="reversed")
 st.sidebar.title('Navigation')
-selection = st.sidebar.radio("Go to", ['О проекте','Визуализация исходных данных', 'Поиск взаимосвязей'])
+selection = st.sidebar.radio("Go to", ['Сводный индекс','Визуализация исходных данных', 'Поиск взаимосвязей', 'О проекте'])
+if selection == 'Сводный индекс':
+	st.header('Сводный индекс эпидемической обстановки')
+	sum_cor2=pd.read_excel('data/index_correlated.xlsx')
+	fig2 = px.scatter(sum_cor2, x='month', y="relative",
+	     size="relative", hover_name=sum_cor2.month,color_discrete_sequence=px.colors.diverging.RdYlBu, 
+	                 color='relative', log_y=True, size_max=100,  hover_data=['absolute'])
+	st.plotly_chart(fig2)
+	st.markdown('Absolute - общее количество коррелирующих с ковидом запросов за месяц')
+	st.markdown('Reative - относительное количество запросов, по сравнению с максимумом')
+	corr_data=pd.read_excel('data/only_correlated_data.xlsx', index_col='month')
+	st.write('Помесячное количество коррелирующих запросов ', corr_data)
 if selection == 'О проекте':
 	st.header('О проекте')
 	st.markdown('Цель данного проекта: поиск математической устойчивой взаимосвязи между релевантной поисковой статистикой россиян и динамикой заболеваемости COVID-19 в РФ')
@@ -27,6 +33,10 @@ if selection == 'О проекте':
 	st.markdown('Научный руководитель: С. Сошников')
 	st.markdown('>Используйте меню слева для перехода на другие страницы сайта')
 if selection == 'Визуализация исходных данных':
+	df_cov_sc=pd.read_excel('data/scaled_data.xlsx')
+	fig = px.line(df_cov_sc, x='month', y='value', color='query', hover_name="query").for_each_trace(lambda t: t.update(name=t.name.split("=")[1]))
+	fig.update_traces(mode='markers+lines')
+	fig.update_layout(legend_traceorder="reversed")
 	st.header('Визуализация данных')
 	st.write('Ниже представлены графики релевантной поисковой семантики и случаев заражения covid-19 и смертей')
 	st.header('Масштабированные данные')
@@ -47,7 +57,7 @@ if selection == 'Поиск взаимосвязей':
     И к полученным результатам применена поправка Бенджамини-Хохберга на множественную проверку гипотез, для повышения надежности результатов. <br>
     Подтвержденные значения корреляций показаны ниже. 
     """, unsafe_allow_html=True)
-	df_cov_true=pd.read_excel('data/corr_only_true_data.xlsx')
+	df_cov_true=pd.read_excel('data/corr_true_data.xlsx')
 	st.write(df_cov_true)
 	st.markdown("""corr - значение корреляции пирсона между A и B<br>
 				p - первичный уровень значимости для данной пары<br>
